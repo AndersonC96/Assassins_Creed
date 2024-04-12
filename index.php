4765bd79e3890ffe73a0332f914605e3a7c7cd54
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-04-12 17:10:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Assassin's Creed - Portal</title>
+        <link rel="icon" href="./IMG/favicon.png" type="image/x-icon" />
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.0.0/dist/tailwind.min.css" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="./CSS/style.css">
